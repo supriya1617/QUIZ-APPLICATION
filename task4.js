8b1320bf0c9ee2db66b786561b981482e3f7cfa9
supriya1617 <supriyapikilika@gmail.com>
2025-05-28 11:11:24 +0530

    Delete task4.js

--- task4.js
+++
@@ -1,7 +0,0 @@
-let progress = 0;
- function increaseProgress() {
-    if (progress < 100) {
-        progress += 20; // increase by 20%
-        document.getElementById("progress").style.width = progress + "%";
-        }
- }
